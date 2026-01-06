abd592a3321696a60643d382809276152edb71a3
sang won <tkddnjs063089@gmail.com>
2026-01-06 17:16:32 +0900

    Remove output folder from git

--- src/teams/dto/create-team.dto.ts
+++ src/teams/dto/create-team.dto.ts
@@ -9,10 +9,10 @@ import {
 } from 'class-validator';
 
 export class CreateTeamDto {
-  @ApiProperty({ description: '팀 이름', example: '핑크팀', maxLength: 50 })
+  @ApiProperty({ description: '팀 이름', example: '핑크팀', maxLength: 255 })
   @IsString()
   @IsNotEmpty({ message: '팀 이름은 필수 입력 항목입니다.' })
-  @MaxLength(50, { message: '팀 이름은 최대 50자 이하여야 합니다.' })
+  @MaxLength(255, { message: '팀 이름은 최대 255자 이하여야 합니다.' })
   teamName: string;
 
   @ApiPropertyOptional({
