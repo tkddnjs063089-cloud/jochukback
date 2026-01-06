63c4d6f99233484015d4603e40943543f2d0fda1
sang won <tkddnjs063089@gmail.com>
2026-01-06 10:52:28 +0900

    fixedMatchRecord

--- src/match-dates/match-dates.module.ts
+++ src/match-dates/match-dates.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchDatesService } from './match-dates.service';
 import { MatchDatesController } from './match-dates.controller';
+import { MatchDates } from './entities/match-date.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MatchDates])],
   controllers: [MatchDatesController],
   providers: [MatchDatesService],
 })
