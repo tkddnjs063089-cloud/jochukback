db1c5c23a66087a28612967d7d43a87e5569a3ba
sang won <tkddnjs063089@gmail.com>
2026-01-06 16:29:44 +0900

    fixedcors

--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
+      'https://eun-yeo-wool.vercel.app',
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
