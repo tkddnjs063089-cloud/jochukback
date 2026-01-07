476a8351c71de03e0bdff161c30b9be8c03b4ef6
sang won <tkddnjs063089@gmail.com>
2026-01-07 11:35:48 +0900

    feat: add match_dates relationship

--- src/match-dates/dto/create-match-date.dto.ts
+++ src/match-dates/dto/create-match-date.dto.ts
@@ -6,14 +6,4 @@ export class CreateMatchDateDto {
   @IsDateString()
   @IsNotEmpty({ message: '경기 날짜는 필수 입력 항목입니다.' })
   eventDate: string;
-
-  @ApiProperty({ description: '경기 장소', example: '서울 월드컵 경기장' })
-  @IsString()
-  @IsNotEmpty({ message: '경기 장소는 필수 입력 항목입니다.' })
-  location: string;
-
-  @ApiProperty({ description: '경기 이름', example: '서울 월드컵 경기' })
-  @IsString()
-  @IsNotEmpty({ message: '경기 이름는 필수 입력 항목입니다.' })
-  eventName: string;
 }
