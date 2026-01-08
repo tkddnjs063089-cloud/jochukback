83475cb7a0700de33972ff2be54fd37a5faad7b1
sang won <tkddnjs063089@gmail.com>
2026-01-08 10:23:32 +0900

    fixedMatches

--- src/matches/dto/create-match.dto.ts
+++ src/matches/dto/create-match.dto.ts
@@ -16,11 +16,9 @@ export class CreateMatchDto {
 
   @ApiProperty({ description: '경기 날짜', example: '2026-01-05' })
   @IsDateString()
-  @IsNotEmpty({ message: '날짜는 필수 입력 항목입니다.' })
-  matchDate: string;
+  matchDate?: string;
   @ApiProperty({ description: '경기 순서', example: 1, minimum: 1 })
   @IsNumber()
-  @IsNotEmpty({ message: '순서는 필수 입력 항목입니다.' })
   @Min(1, { message: '순서는 1 이상이어야 합니다.' })
-  matchOrder: number;
+  matchOrder?: number;
 }
