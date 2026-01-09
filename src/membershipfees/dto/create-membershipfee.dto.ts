622fca073b872a66dd0480ea2bd174ec2d0e4747
sang won <tkddnjs063089@gmail.com>
2026-01-09 15:03:01 +0900

    fixedMembershipFees

--- src/membershipfees/dto/create-membershipfee.dto.ts
+++ src/membershipfees/dto/create-membershipfee.dto.ts
@@ -11,7 +11,6 @@ import { IsNotEmpty } from 'class-validator';
 export class CreateMembershipFeeDto {
   @ApiProperty({ description: '선수 이름', example: '홍길동', maxLength: 50 })
   @IsString()
-  @IsNotEmpty({ message: '이름은 필수 입력 항목입니다.' })
   @MinLength(1, { message: '이름은 최소 2자 이상이어야 합니다.' })
   @MaxLength(50, { message: '이름은 최대 50자 이하여야 합니다.' })
   playerName: string;
